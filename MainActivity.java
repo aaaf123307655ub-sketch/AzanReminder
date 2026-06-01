c5977797d96686f99c07b7076cb2703fbdead35d
aaaf123307655ub-sketch <aaaf123307655ub@gmail.com>
2026-06-01 16:35:04 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.azan;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("مرحباً بك في تطبيق الأذان");
+        setContentView(tv);
+    }
+}
+
